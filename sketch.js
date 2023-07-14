1987e1805d22b46ad6923fdff99034461c0db2bd
ttmmd55 <chenshuting47@gmail.com>
2023-07-14 11:24:50 +0100

    update

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var video;
 var vScale = 16;
 
 function setup() {
-    var canvas = createCanvas(308, 305);
+    var canvas = createCanvas(778, 1044);
     pixelDensity(1);
     video = createCapture(VIDEO);
     video.size(width / vScale, height / vScale);
